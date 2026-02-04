e6ad7c6204974e6b04ebbd65cc030cb3bff7682d
Margrethe1316 <maso0012@stud.kea.dk>
2026-02-04 11:44:00 +0100

    breadcrumb done

---
+++ breadcrumb_navigation/js.js
@@ -0,0 +1,34 @@
+const bc = [
+  { name: "Hvidevarer", link: "/hvidevarer" },
+  { name: "Vaskemaskiner", link: "/hvidevarer/vaskemaskiner" },
+  { name: "Bosch", link: "/hvidevarer/vaskemaskiner/bosch/" },
+];
+
+// Finder containeren og knappen //
+
+const breadcrumbContainer = document.getElementById("breadcrumb");
+const button = document.getElementById("generate");
+
+// Funktion der genererer breadcrumb //
+
+function generateBreadcrumb() {
+  breadcrumbContainer.innerHTML = "";
+
+  bc.forEach((item, index) => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    // Det sidste punkt skal være tekst //
+    if (index === bc.length - 1) {
+      li.textContent = item.name;
+    } else {
+      a.href = item.link;
+      a.textContent = item.name;
+      li.appendChild(a);
+    }
+
+    breadcrumbContainer.appendChild(li);
+  });
+}
+
+// Eventlistener på knappen //
+button.addEventListener("click", generateBreadcrumb);
